005f4c8f688f9189d1c00580163b377d1cba620a
Kaniadzic <szymon.kania@edu.uekat.pl>
2021-02-23 12:13:45 +0100

    Presentation: improvements in quiz result view

--- Models/Entity/QuestionsAndAnswers.cs
+++ Models/Entity/QuestionsAndAnswers.cs
@@ -15,6 +15,7 @@ namespace HelloWORD.Models.Entity
         public string AnswerB { get; set; }
         public string AnswerC { get; set; }
         public string AnswerD { get; set; }
+        public string Picture { get; set; }
 
     }
 }

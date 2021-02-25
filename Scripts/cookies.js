6b71255bf1e0e06d771dd27ba1c204db8e47daa6
Kaniadzic <szymon.kania@edu.uekat.pl>
2021-02-25 14:18:45 +0100

    Feature: cookies alert

---
+++ Scripts/cookies.js
@@ -0,0 +1,23 @@
+﻿window.onload = checkCookiesAgreement;
+
+function checkCookiesAgreement() {
+    cookiesAgreed = localStorage.getItem('HelloWORDCookiesAgreed');
+
+    if (!cookiesAgreed) {
+        cookiesAlert();
+    }
+}
+
+function cookiesAlert() {
+    let acceptCookiesButton = document.getElementById('acceptCookies');
+    let cookiesAlert = document.getElementById('cookiesAlert');
+    cookiesAlert.style.display = "flex";
+
+    acceptCookiesButton.addEventListener('click', agreeToCookies);
+}
+
+function agreeToCookies() {
+    localStorage.setItem('HelloWORDCookiesAgreed', 'True');
+    let cookiesAlert = document.getElementById('cookiesAlert');
+    cookiesAlert.style.display = "none";
+}

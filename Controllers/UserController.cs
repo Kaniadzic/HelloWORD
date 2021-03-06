1051ebf4c20d56f32c960f30a2399f8108720570
Kaniadzic <szymon.kania@edu.uekat.pl>
2021-03-06 16:08:37 +0100

    Presentation: login page and temporary favicon

---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWORD.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Logon()
+        {
+            return View();
+        }
+    }
+}

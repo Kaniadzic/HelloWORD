e4867fa10f7fbecfcbb9cfb9db5f7770cf92cf6c
Kaniadzic <szymon.kania@edu.uekat.pl>
2021-03-06 16:22:00 +0100

    Feature: user login and password model, form now posts user login data to controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,7 +8,20 @@ namespace HelloWORD.Controllers
 {
     public class UserController : Controller
     {
+        [HttpGet]
         public ActionResult Logon()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Logon(string login, string password)
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult Register()
         {
             return View();
         }

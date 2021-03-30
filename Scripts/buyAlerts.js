4afaac7b0f7681f44aa620d7580fb906f62da37f
Kaniadzic <szymon.kania@edu.uekat.pl>
2021-03-30 14:53:49 +0200

    Presentation: buy premium panel

---
+++ Scripts/buyAlerts.js
@@ -0,0 +1,13 @@
+﻿window.onload = dupa;
+
+function dupa() {
+    let buttons = document.querySelectorAll(".premium-panel");
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", showAlert);
+    }
+}
+
+function showAlert() {
+    window.alert("Ta funkcja zostanie zaimplementowana w pełnej, komercyjnej wersji oprogramowania HelloWORD");
+}

4fbb2bec9e030424b83c563cecb144d8bd17e2c7
Kaniadzic <szymon.kania@edu.uekat.pl>
2021-04-10 12:02:58 +0200

    Logic: user history entity and logic

---
+++ Models/Entity/UserHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWORD.Models.Entity
+{
+    public class UserHistory
+    {
+        public int UserID { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public int Score { get; set; }
+        public bool Passed { get; set; }
+    }
+}
---
+++ Models/Logic/HistoryLogic.cs
@@ -0,0 +1,35 @@
+﻿using HelloWORD.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace HelloWORD.Models.Logic
+{
+    public class HistoryLogic
+    {
+        public void AddHistory(UserHistory userData)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DatabaseContext"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_SelectQuestionsNumbers", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("UserID", userData.UserID);
+                cmd.Parameters.AddWithValue("Date", userData.Date);
+                cmd.Parameters.AddWithValue("Category", userData.Category);
+                cmd.Parameters.AddWithValue("Type", userData.Type);
+                cmd.Parameters.AddWithValue("Score", userData.Score);
+                cmd.Parameters.AddWithValue("Passed", userData.Passed);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                con.Close();
+            }
+        }
+    }
+}

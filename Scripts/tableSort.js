d2a6554bd05d0345ecc87e9bc7e39220ea6fc037
Kaniadzic <szymon.kania@edu.uekat.pl>
2021-04-16 14:57:26 +0200

    Feature: sorting and paging history tables with DataTable

--- Scripts/tableSort.js
+++ Scripts/tableSort.js
@@ -1,6 +1,31 @@
 ﻿function dupa() {
-    let t = $('#quizTable');
-    console.log(t);
+    $('#quizTable').DataTable({
+        paging: true,
+        searching: false,
+        info: false,
+        lengthChange: true,
+        language: {
+            lengthMenu: "Pokaż _MENU_ rekordów",
+            paginate: {
+                "previous": "wstecz",
+                "next": "dalej"
+            }
+        }
+    });
+
+    $('#examTable').DataTable({
+        paging: true,
+        searching: false,
+        info: false,
+        lengthChange: true,
+        language: {
+            lengthMenu: "Pokaż _MENU_ rekordów",
+            paginate: {
+                "previous": "wstecz",
+                "next": "dalej"
+            }
+        }
+    });
 }
 
 window.onload = dupa;
